1c69fc89288467f89ec116102fa061e928f6f60e
werar <werar@go2.pl>
2015-08-08 22:27:40 +0200

    pierwsze wersje podstawowych programów

---
+++ software/LUA/init_and_setup/createConfFile.lua
@@ -0,0 +1,17 @@
+-- Initialise config file 
+
+-- This program has two modes based on state JP8 jumper.
+-- Copyright [2015] [werar@go2.pl https://github.com/werar]
+--Licensed under the Apache License, Version 2.0 (the "License")  http://www.apache.org/licenses/LICENSE-2.0
+
+function writeConf(params)
+  file.open("conf.txt","w")
+  file.writeline("ssidName") -- wifi ssid name
+  file.writeline("wifi_password") -- wifi password
+  file.writeline("184.106.153.149") -- thinkspeak ip
+  file.writeline("thinkspeak_token") -- thinkspeak token
+  file.close()
+  print("Config is saved");
+end
+
+writeConf();
