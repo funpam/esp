2ad80e884ac9e6401dbbbcf8ac28ffc61cc50fe3
werar <werar@go2.pl>
2015-08-09 18:19:04 +0200

    prosty przykład jak zapalić diodeLED

---
+++ software/LUA/init_and_setup/blinkLed.lua
@@ -0,0 +1,27 @@
+-- Blink LED2 on funpam board
+-- 
+-- Copyright [2015] [werar@go2.pl https://github.com/werar]
+--Licensed under the Apache License, Version 2.0 (the "License")  http://www.apache.org/licenses/LICENSE-2.0
+
+--very simple example 
+function blinkLed(pin) 
+    gpio.write(pin,gpio.HIGH)
+	tmr.delay(1000000)
+    gpio.write(pin,gpio.LOW)
+    tmr.delay(1000000)	
+end
+
+--PWM version. Firmware must to have PWM module included (not used in basic example)
+function blinkLedPWM(pin)  
+    pwm.setduty(pin, 512)
+	pwm.setup(pin,1,512)
+	pwm.start(pin)
+end
+
+ 
+-- main program
+local _led2Port=5; --5 means GPIO14
+gpio.mode(_led2Port,gpio.OUTPUT) --set GPIO14 as output
+
+-- use timer0 to periodically call function 
+tmr.alarm(0, 60000, 1, function() blinkLed(_led2Port) end )
